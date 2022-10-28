6d8b8097be744714466d967ac57f0fb8b42fc2b6
Guttapepper <112008474+Guttapepper@users.noreply.github.com>
2022-10-28 13:02:40 +0300

    Sounds+Zombies

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GROUND_TYPE
+        {
+            static const AkUniqueID GROUP = 4040317175U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DRY = 630539344U;
+                static const AkUniqueID WET = 1181096339U;
+            } // namespace SWITCH
+        } // namespace GROUND_TYPE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
